318257e50f74bb7f858c99ad2e2c22c923248f55
hamed952 <hamedbraooie289@gmail.com>
2025-07-24 01:33:45 +0330

    Update us.py

--- us.py
+++ us.py
@@ -18,19 +18,19 @@ Sdf = """
 for i in Sdf:
   sleep(0.02)
   print(i,end='',flush=True)
-print()
+printb((hhsvdbbduhd
 print(Fore.RED+" Play Ok ")
-sleep(2.1)
+slee///Hbsccsyvsbs
 print()
 def random_string_genera_variable_size(min_size, max_size, allowed_chars):
   return ''.join(random.choice(allowed_chars) for x in range(randint(min_size, max_size)))
 chars = string.ascii_letters + string.punctuation
 print(Fore.GREEN+"    "+"{"+"_"*50+"}")
 print("     "+Fore.YELLOW+'Code Account >> ', random_string_genera_variable_size(6, 12, chars))
-print(Fore.GREEN+"    "+"{"+"_"*50+"}")
+print(Fore.GREENHsnnsubenn  "+"{"+"_"*50+"}")
 print()
 sleep(1.0)
-print(Fore.GREEN+"""
+print(Fore.GREEN+"""...genjduv
     Ok 
      """)
-input()
+input()..
